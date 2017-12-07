bf3c43cb0f9eff4c5e4e459a830b9ee6683969f8
AadityaJ <aadityajamuar@gmail.com>
2017-12-07 23:25:42 +0530

    adding example

---
+++ examples/add_state/mClient.cpp
@@ -0,0 +1,14 @@
+#include "../../includes/mClient.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	while(1){
+		Client c(90190);
+		int a,b;
+		cin>>a>>b;
+		string snd=to_string(a)+","+to_string(b);
+		c.Send(snd);
+		cout<<c.receive()<<endl;
+	}
+	return 0;
+}
---
+++ examples/add_state/mServer.cpp
@@ -0,0 +1,50 @@
+#include "../../includes/mServer.h"
+#include "../../includes/state.h"
+#include <utility>
+using namespace std;
+class myClass{
+public:
+	int a;
+	int b;
+	myClass(int a,int b){
+		this->a=a;
+		this->b=b;
+	}
+	string to_str(){
+		return to_string(this->a)+","+to_string(this->b);
+	}
+	int add(){
+		return this->a+this->b;
+	}
+};
+pair<string,string> split(string a){
+	string temp1="",temp2="";
+	int i=0;
+	for(;a[i]!=',';i++){temp1.push_back(a[i]);}
+	i++;
+	for(;i!=a.length();i++){temp2.push_back(a[i]);}
+	return make_pair(temp1,temp2);
+}
+int main(int argc , char *argv[]){
+	Server s;
+	state<myClass,int> st;
+	s.listen_(90190);
+	string val;
+	while(1){
+		int ans;
+		val=s.receive();
+		pair<string,string> pr=split(val);
+		myClass mc(stoi(pr.first),stoi(pr.second));
+		int ptr=st.seek(mc);
+		if(ptr==NULL){
+			//cout<<"Not in cache\n";
+			ans=mc.add();
+			st.add(mc,ans);
+		}else{
+			//cout<<"In cache\n";
+			ans=ptr;
+		}
+		s.send_(to_string(ans));
+	}
+    return 0;
+}
